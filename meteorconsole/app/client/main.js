e1442790e5015f43f69448c30308e7f1821931c2
Wojciech Piotrowiak <wojciech.piotrowiak@gmail.com>
2016-05-21 21:11:10 +0200

    cleanup

--- meteorconsole/app/client/main.js
+++ meteorconsole/app/client/main.js
@@ -40,32 +40,7 @@ Template.hello.events({
 			event.target.value="";
 			});
 		}
-	},
-
-	"submit .command-run": function (event) {
-		var command = event.target.url.value;
-		Meteor.call('action', command,function(error,result){
-		$('#console').val(result.stdout);
-		});
-		event.target.url.value="";
-		return false;
-	},
-
-	"submit .download": function (event) {
-		var url = event.target.url.value;
-		var id=Random.id();
-
-		var cmd='cd /home/wpiotrowiak && ';
-		cmd+='mkdir '+id+' && ';
-		cmd+='cd '+id+' && ';
-		cmd+='wget '+url+' && ';
-		cmd+='cd .. && ';
-		cmd+='zip '+id+' '+id+' && ';
-		cmd+='rm -Rf '+id;
-
-		Meteor.call('action', cmd,function(error,result){
-
-		});
-		return false;
 	}
+}
+
   });
